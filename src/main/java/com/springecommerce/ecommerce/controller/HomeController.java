73f56a09acbd8d35eb5e5174809b559cdcc67d87
Jean Reyes <eliasreyes1601@gmail.com>
2022-10-22 14:53:19 -0500

    Vista de cada producto

--- src/main/java/com/springecommerce/ecommerce/controller/HomeController.java
+++ src/main/java/com/springecommerce/ecommerce/controller/HomeController.java
@@ -2,17 +2,22 @@ package com.springecommerce.ecommerce.controller;
 
 import com.springecommerce.ecommerce.model.Producto;
 import com.springecommerce.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private ProductoService productoService;
 
@@ -21,4 +26,14 @@ public class HomeController {
         model.addAttribute("productos", productoService.finAll());
         return "usuario/home";
     }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome(Model model ,@PathVariable Integer id){
+        Producto producto = new Producto();
+        producto = productoService.get(id).get();
+        model.addAttribute("producto", producto);
+        return  "usuario/productohome";
+    }
+
+
 }
