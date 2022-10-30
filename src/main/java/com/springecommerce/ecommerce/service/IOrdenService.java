cce541434db9be0de210a909f833aa21a1bb845a
Jean Reyes <eliasreyes1601@gmail.com>
2022-10-30 17:04:11 -0500

    Generar numero orden

--- src/main/java/com/springecommerce/ecommerce/service/IOrdenService.java
+++ src/main/java/com/springecommerce/ecommerce/service/IOrdenService.java
@@ -3,8 +3,12 @@ package com.springecommerce.ecommerce.service;
 import com.springecommerce.ecommerce.model.Orden;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface IOrdenService {
+
+    List<Orden> findAll();
     Orden save(Orden orden);
 
 }
--- src/main/java/com/springecommerce/ecommerce/service/OrdenServiceImp.java
+++ src/main/java/com/springecommerce/ecommerce/service/OrdenServiceImp.java
@@ -5,6 +5,9 @@ import com.springecommerce.ecommerce.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImp implements IOrdenService {
 
@@ -12,8 +15,37 @@ public class OrdenServiceImp implements IOrdenService {
     private IOrdenRepository iOrdenRepository;
 
     @Override
+    public List<Orden> findAll() {
+        return iOrdenRepository.findAll();
+    }
+
+    @Override
     public Orden save(Orden orden) {
 
         return iOrdenRepository.save(orden);
+    }
+
+    public String generarNumeroOrden(){
+        Integer numeroOrden =0;
+        String numeroConcat = "";
+
+        List<Orden> ordenes = findAll();
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()){
+            numeroOrden=1;
+        }else{
+            numeroOrden=numeros.stream().max(Integer::compare).get();
+            numeroOrden++;
+        }
+
+        numeroConcat=numeroOrden.toString();
+        for (int i=numeroConcat.length(); i<10 ; i++ ){
+            numeroConcat = "0"+ numeroConcat;
+        }
+
+        return numeroConcat;
     }
 }
